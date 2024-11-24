256c9445b27f5853b99f17e4089e52aac16447e3
Daniel da Silva <mail@danieldasilva.org>
2024-11-24 04:07:50 -0500

    Add automatically generated __version__

--- rbinvariantslib/__init__.py
+++ rbinvariantslib/__init__.py
@@ -1,1 +1,7 @@
 
+try:
+    from ._version import __version__
+    from ._version import version_tuple
+except ImportError:
+    __version__ = "unknown"
+    version_tuple = (0, 0, "unknown version")
